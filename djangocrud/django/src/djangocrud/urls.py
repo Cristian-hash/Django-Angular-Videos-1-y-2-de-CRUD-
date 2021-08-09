064411e1f2ba1c4634d060e594be0f2fbedd5522
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-08-08 22:37:07 -0500

    formulario

--- djangocrud/django/src/djangocrud/urls.py
+++ djangocrud/django/src/djangocrud/urls.py
@@ -23,9 +23,6 @@ router = routers.DefaultRouter()
 router.register(r'movies', views.MovieViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
+ 
 ]
 #
