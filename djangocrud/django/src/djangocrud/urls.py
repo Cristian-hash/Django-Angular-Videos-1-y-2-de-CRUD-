f9396740a7141b970da226b739dec34b7bf32dcf
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-08-09 10:26:03 -0500

    funcion eliminar pelicula-Angular

--- djangocrud/django/src/djangocrud/urls.py
+++ djangocrud/django/src/djangocrud/urls.py
@@ -5,8 +5,7 @@ from django.urls import include, path
 from rest_framework import routers
 from api import views
 
-router = routers.DefaultRouter()
-router.register(r'movies', views.MovieViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
